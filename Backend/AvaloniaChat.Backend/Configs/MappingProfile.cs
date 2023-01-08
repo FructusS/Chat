14951d09517aa85cc8d0f8a63ecaebc053fded88
FructusS <afanasev10056@gmail.com>
2023-01-08 22:07:08 +0300

    add automapper

---
+++ Backend/AvaloniaChat.Backend/Configs/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AvaloniaChat.Backend.Models;
+
+
+namespace AvaloniaChat.Backend.Configs
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+           // CreateMap<UpdateUserModel, User>().ReverseMap();
+        }
+    }
+}
