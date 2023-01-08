e17b941ffa9b9a8a17976fd5cf3187ee3979381b
FructusS <afanasev10056@gmail.com>
2023-01-08 22:14:10 +0300

    create igroupservice

---
+++ Backend/AvaloniaChat.Backend/Services/Interfaces/IGroupService.cs
@@ -0,0 +1,11 @@
+﻿using AvaloniaChat.Backend.Models;
+
+namespace AvaloniaChat.Backend.Interfaces
+{
+    public interface IGroupService
+    {
+        Task<Group> CreateGroup(Group group);
+        Task DeleteGroup(Group group);
+        Task<Group> UpdateGroup(Group group);
+    }
+}
