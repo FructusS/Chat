f395b148f6101ba3a53c5477481f3b82a6b8707f
FructusS <afanasev10056@gmail.com>
2023-01-08 22:12:22 +0300

    add createusermodel

---
+++ Backend/AvaloniaChat.Backend/Models/UserModels/CreateUserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvaloniaChat.Backend.Models.UserModels
+{
+    public class CreateUserModel
+    {
+        [Required, MaxLength(60), MinLength(7)]        
+        public string Email { get; set; } = null!;
+
+        [Required, MaxLength(20), MinLength(3)]
+        public string Username { get; set; } = null!;
+
+        [Required, MaxLength(100), MinLength(6)]
+        public string Password { get; set; } = null!;
+
+        [Required, MaxLength(100), MinLength(6), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
