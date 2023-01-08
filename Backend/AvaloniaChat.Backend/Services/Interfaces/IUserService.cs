0ed2da0b545c0f672c76038a259bb459e8da73b2
FructusS <afanasev10056@gmail.com>
2023-01-08 22:14:21 +0300

    create iuserservice

---
+++ Backend/AvaloniaChat.Backend/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using AvaloniaChat.Backend.Models;
+using AvaloniaChat.Backend.Models.UserModels;
+
+namespace AvaloniaChat.Backend.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> CreateUser(CreateUserModel createUser);
+        Task<User> UpdateUser(UpdateUserModel updateUser);
+        Task DeleteUser(int id);
+    }
+}
