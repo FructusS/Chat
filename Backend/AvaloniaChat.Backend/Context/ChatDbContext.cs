1b7ae8a80e45d508ea00b09530afbe9d9e8b8ca2
FructusS <afanasev10056@gmail.com>
2023-01-08 22:07:25 +0300

    update models

--- Backend/AvaloniaChat.Backend/Context/ChatDbContext.cs
+++ Backend/AvaloniaChat.Backend/Context/ChatDbContext.cs
@@ -10,7 +10,7 @@ public partial class ChatDbContext : DbContext
 {
     public ChatDbContext()
     {
-        
+
     }
 
     public ChatDbContext(DbContextOptions<ChatDbContext> options)
@@ -18,15 +18,14 @@ public partial class ChatDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Gender> Genders { get; set; }
 
-    public virtual DbSet<Group> Groups { get; set; }
 
-    public virtual DbSet<Message> Messages { get; set; }
+    public virtual DbSet<Gender> Genders { get; set; } = null!;
+    public virtual DbSet<Group> Groups { get; set; } = null!;
+    public virtual DbSet<Message> Messages { get; set; } = null!;
+    public virtual DbSet<User> Users { get; set; } = null!;
+    public virtual DbSet<UserGroup> UserGroups { get; set; } = null!;
 
-    public virtual DbSet<User> Users { get; set; }
-
-    public virtual DbSet<UserGroup> UserGroups { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -34,9 +33,8 @@ public partial class ChatDbContext : DbContext
         {
             entity.ToTable("Gender");
 
-            entity.Property(e => e.GenderId)
-                .ValueGeneratedNever()
-                .HasColumnName("gender_id");
+            entity.Property(e => e.GenderId).HasColumnName("gender_id");
+
             entity.Property(e => e.GenderTitle)
                 .HasMaxLength(50)
                 .IsUnicode(false)
@@ -47,12 +45,12 @@ public partial class ChatDbContext : DbContext
         {
             entity.ToTable("Group");
 
-            entity.Property(e => e.GroupId)
-                .ValueGeneratedNever()
-                .HasColumnName("group_id");
+            entity.Property(e => e.GroupId).HasColumnName("group_id");
+
             entity.Property(e => e.GroupImage)
                 .HasColumnType("image")
                 .HasColumnName("group_image");
+
             entity.Property(e => e.GroupTitle)
                 .HasMaxLength(50)
                 .HasColumnName("group_title");
@@ -62,17 +60,14 @@ public partial class ChatDbContext : DbContext
         {
             entity.ToTable("Message");
 
-            entity.Property(e => e.MessageId)
-                .HasMaxLength(10)
-                .IsFixedLength()
-                .HasColumnName("message_id");
-            entity.Property(e => e.MessageText).HasColumnName("message_text");
-            entity.Property(e => e.UsergroupId)
-                .HasMaxLength(10)
-                .IsFixedLength()
-                .HasColumnName("usergroup_id");
+            entity.Property(e => e.MessageId).HasColumnName("message_id");
 
-            entity.HasOne(d => d.Usergroup).WithMany(p => p.Messages)
+            entity.Property(e => e.MessageText).HasColumnName("message_text");
+
+            entity.Property(e => e.UsergroupId).HasColumnName("usergroup_id");
+
+            entity.HasOne(d => d.Usergroup)
+                .WithMany(p => p.Messages)
                 .HasForeignKey(d => d.UsergroupId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Message_UserGroup");
@@ -82,33 +77,39 @@ public partial class ChatDbContext : DbContext
         {
             entity.ToTable("User");
 
-            entity.Property(e => e.UserId)
-                .ValueGeneratedNever()
-                .HasColumnName("user_id");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
             entity.Property(e => e.Email)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("email");
+
             entity.Property(e => e.FirstName)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("first_name");
+
             entity.Property(e => e.GenderId).HasColumnName("gender_id");
+
             entity.Property(e => e.LastName)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("last_name");
+
             entity.Property(e => e.Logo)
                 .HasColumnType("image")
                 .HasColumnName("logo");
+
             entity.Property(e => e.PasswordHash)
                 .HasColumnType("text")
                 .HasColumnName("password_hash");
+
             entity.Property(e => e.Username)
                 .HasMaxLength(50)
                 .HasColumnName("username");
 
-            entity.HasOne(d => d.Gender).WithMany(p => p.Users)
+            entity.HasOne(d => d.Gender)
+                .WithMany(p => p.Users)
                 .HasForeignKey(d => d.GenderId)
                 .HasConstraintName("FK_User_Gender");
         });
@@ -117,18 +118,19 @@ public partial class ChatDbContext : DbContext
         {
             entity.ToTable("UserGroup");
 
-            entity.Property(e => e.UsergroupId)
-                .HasMaxLength(10)
-                .IsFixedLength()
-                .HasColumnName("usergroup_id");
+            entity.Property(e => e.UsergroupId).HasColumnName("usergroup_id");
+
             entity.Property(e => e.GroupId).HasColumnName("group_id");
+
             entity.Property(e => e.UserId).HasColumnName("user_id");
 
-            entity.HasOne(d => d.Group).WithMany(p => p.UserGroups)
+            entity.HasOne(d => d.Group)
+                .WithMany(p => p.UserGroups)
                 .HasForeignKey(d => d.GroupId)
                 .HasConstraintName("FK_UserGroup_Group");
 
-            entity.HasOne(d => d.User).WithMany(p => p.UserGroups)
+            entity.HasOne(d => d.User)
+                .WithMany(p => p.UserGroups)
                 .HasForeignKey(d => d.UserId)
                 .HasConstraintName("FK_UserGroup_User");
         });
@@ -137,4 +139,5 @@ public partial class ChatDbContext : DbContext
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
 }
