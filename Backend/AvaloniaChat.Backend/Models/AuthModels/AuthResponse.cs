f546ddc29861abfdd0f7e65a085f42dc1ff62a99
FructusS <afanasev10056@gmail.com>
2023-01-12 23:30:24 +0300

    create auth response

---
+++ Backend/AvaloniaChat.Backend/Models/AuthModels/AuthResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace AvaloniaChat.Backend.Models.AuthModels
+{
+    public class AuthResponse
+    {
+        private string _accesstoken;
+        //      private string _refreshstoken;
+
+        public string AccessToken
+        {
+            get { return _accesstoken; }
+            set { _accesstoken = value; }
+        }
+        //public string RefreshToken
+        //{
+        //    get { return _refreshstoken; }
+        //    set { _refreshstoken = value; }
+        //}
+    }
+}
