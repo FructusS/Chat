049c15bda3dbcdff21609e8beaae880ff5fd9151
FructusS <afanasev10056@gmail.com>
2023-01-12 23:33:58 +0300

    create auth service

---
+++ Backend/AvaloniaChat.Backend/Services/Implimentations/AuthService.cs
@@ -0,0 +1,62 @@
+﻿using AvaloniaChat.Backend.Configs;
+using AvaloniaChat.Backend.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AvaloniaChat.Backend.Services.Implimentations
+{
+    public class AuthService : IAuthService
+    {
+        private readonly JwtConfig _jwtConfig;
+
+
+        public AuthService(IOptions<JwtConfig> jwtConfig)
+        {
+            _jwtConfig = jwtConfig.Value;
+        }
+
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var tokeOptions = new JwtSecurityToken(
+                issuer: _jwtConfig.Issuer,
+                audience: _jwtConfig.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_jwtConfig.AccessTokenLifeTime),
+                signingCredentials: new SigningCredentials(_jwtConfig.SymmetricSecurityKey(), SecurityAlgorithms.HmacSha256)
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            return tokenString;
+        }
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false, //you might want to validate the audience and issuer depending on your use case
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _jwtConfig.SymmetricSecurityKey(),
+                ValidateLifetime = false //here we are saying that we don't care about the token's expiration date
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+            return principal;
+        }
+    }
+}
