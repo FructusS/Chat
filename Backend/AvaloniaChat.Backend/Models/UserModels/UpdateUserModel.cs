48e1d6cf129eb2ee7a98dd402ec2c1f304371b02
FructusS <afanasev10056@gmail.com>
2023-01-12 23:31:09 +0300

    create model for update user

--- Backend/AvaloniaChat.Backend/Models/UserModels/UpdateUserModel.cs
+++ Backend/AvaloniaChat.Backend/Models/UserModels/UpdateUserModel.cs
@@ -1,7 +1,11 @@
-﻿namespace AvaloniaChat.Backend.Models.UserModels
+﻿using System.ComponentModel.DataAnnotations;
+namespace AvaloniaChat.Backend.Models.UserModels
 {
     public class UpdateUserModel
     {
-
+        public string Username { get; set; } = null!;
+        public string? FirstName { get; set; }
+        public byte[]? Logo { get; set; }
+        public string? LastName { get; set; }
     }
 }
