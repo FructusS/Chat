3ba9a7a2f8709a1311c98f1b209aad5c618933f6
FructusS <afanasev10056@gmail.com>
2023-01-22 18:26:17 +0300

    add usergroupservice

---
+++ Backend/AvaloniaChat.Backend/Services/Implimentations/UserGroupService.cs
@@ -0,0 +1,32 @@
+﻿using AvaloniaChat.Backend.Context;
+using AvaloniaChat.Backend.Models;
+using AvaloniaChat.Backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvaloniaChat.Backend.Services.Implimentations
+{
+    public class UserGroupService : IUserGroupService
+    {
+        private readonly ChatDbContext _chatDbContext;
+
+        public UserGroupService(ChatDbContext chatDbContext)
+        {
+            _chatDbContext = chatDbContext;
+        }
+
+        public async Task<UserGroup> AddUserFromGroup(int userId, int groupId)
+        {
+            var group = new UserGroup { UserId = userId, GroupId = groupId };
+            await  _chatDbContext.UserGroups.AddAsync(group);
+            await _chatDbContext.SaveChangesAsync();
+            return group;
+        }
+
+        public async Task DeleteUserFromGroup(int userId, int groupId)
+        {
+            var useringroup = _chatDbContext.UserGroups.Where(x=> x.UserId == userId && x.GroupId == groupId).First();
+            _chatDbContext.UserGroups.Remove(useringroup);
+            await _chatDbContext.SaveChangesAsync();
+        }
+    }
+}
