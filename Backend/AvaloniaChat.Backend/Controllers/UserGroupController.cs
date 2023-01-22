6fca0d6ca9deea6935a0f5f2a99a720c97573485
FructusS <afanasev10056@gmail.com>
2023-01-22 18:26:32 +0300

    create usergroup controller

---
+++ Backend/AvaloniaChat.Backend/Controllers/UserGroupController.cs
@@ -0,0 +1,32 @@
+﻿using AvaloniaChat.Backend.Models;
+using AvaloniaChat.Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvaloniaChat.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserGroupController : ControllerBase
+    {
+        private readonly IUserGroupService _userGroupService;
+
+        public UserGroupController(IUserGroupService userGroupService)
+        {
+            _userGroupService = userGroupService;
+        }
+
+        [HttpPost]
+        [Route("adduser")]
+        public async Task<UserGroup> AddUserFromGroup([FromQuery]int userId, [FromQuery]int groupId)
+        {
+            return await _userGroupService.AddUserFromGroup(userId, groupId);
+        }
+        [HttpPost]
+        [Route("deleteuser")]
+        public async Task DeleteUserFromGroup([FromQuery]int userId, [FromQuery]int groupId)
+        {
+            await _userGroupService.DeleteUserFromGroup(userId, groupId);
+            
+        }
+    }
+}
