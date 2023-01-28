61b16abceab84e72e753a7accbe957d9591b70c0
FructusS <afanasev10056@gmail.com>
2023-01-28 20:34:21 +0300

    create message controller

---
+++ Backend/AvaloniaChat.Backend/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using AvaloniaChat.Backend.Models;
+using AvaloniaChat.Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AvaloniaChat.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public MessagesController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet("{groupId}")]
+        public async Task<List<Message>> GetMessages(int groupId)
+        {
+
+            return await _messageService.GetMessages(groupId);
+        }
+
+       
+    }
+}
