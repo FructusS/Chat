f931681f57afabee19bebf55630f590b6ce495e3
FructusS <afanasev10056@gmail.com>
2023-01-28 20:35:23 +0300

    create message service

---
+++ Backend/AvaloniaChat.Backend/Services/Implimentations/MessageService.cs
@@ -0,0 +1,22 @@
+﻿using AvaloniaChat.Backend.Context;
+using AvaloniaChat.Backend.Models;
+using AvaloniaChat.Backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvaloniaChat.Backend.Services.Implimentations
+{
+    public class MessageService : IMessageService
+    {
+        private readonly ChatDbContext _chatDbContext;
+
+        public MessageService(ChatDbContext chatDbContext)
+        {
+            _chatDbContext = chatDbContext;
+        }
+
+        public Task<List<Message>> GetMessages(int userGroupId)
+        {
+            return _chatDbContext.Messages.Take(20).ToListAsync();
+        }
+    }
+}
---
+++ Backend/AvaloniaChat.Backend/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using AvaloniaChat.Backend.Models;
+
+namespace AvaloniaChat.Backend.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        Task<List<Message>> GetMessages(int userGroupId);
+    }
+}
