9be78ef7586249454b720e4bdf00d9419f3a18e2
FructusS <afanasev10056@gmail.com>
2023-06-13 00:33:08 +0300

    mapper for user

---
+++ Backend/AvaloniaChat.Backend/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using AvaloniaChat.Backend.Models;
+using AvaloniaChat.Backend.Models.UserModels;
+using ViewModels;
+
+namespace AvaloniaChat.Backend.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, MainUserViewModel>()
+                .ForMember(x => x.Username, opt => opt.MapFrom(x => x.Username))
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(x => x.FirstName))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(x => x.LastName))
+                .ForMember(x => x.Logo, opt => opt.MapFrom(x => x.Logo));
+        }
+    }
+}
+ 
