fcf5180d7e1615ff4a2f03c4bdeed482e005316f
FructusS <afanasev10056@gmail.com>
2023-06-13 00:32:55 +0300

    model for user registration

---
+++ Shared/ViewModels/RegistrationUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaChat.ViewModels
+{
+    internal class RegistrationUserViewModel
+    {
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
