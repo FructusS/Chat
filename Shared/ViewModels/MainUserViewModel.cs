7e3e322677bb1c369be63c8887bdaa12e641672c
FructusS <afanasev10056@gmail.com>
2023-06-13 00:33:21 +0300

    user viewmodel

---
+++ Shared/ViewModels/MainUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ViewModels
+{
+    public class MainUserViewModel
+    {
+        public string Username { get; set; }
+        public string? FirstName { get; set; }
+        public byte[]? Logo { get; set; }
+        public string? LastName { get; set; }
+    }
+}
