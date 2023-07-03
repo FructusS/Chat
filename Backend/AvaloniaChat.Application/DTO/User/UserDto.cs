d8e0317cbeb7544f1d2dad43f013b530284499f2
FructusS <afanasev10056@gmail.com>
2023-07-03 22:49:39 +0300

    remove user group

--- Backend/AvaloniaChat.Application/DTO/User/UserDto.cs
+++ Backend/AvaloniaChat.Application/DTO/User/UserDto.cs
@@ -4,12 +4,8 @@ namespace AvaloniaChat.Application.DTO.User;
 
 public class UserDto
 {
-
-    public int UserId { get; set; }
     public string Username { get; set; } = null!;
     public string? FirstName { get; set; }
     public byte[]? Logo { get; set; }
     public string? LastName { get; set; }
-    public virtual List<UserGroup> UserGroups { get; set; }
-
 }
