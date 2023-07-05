00f9cd6a8094d06f4c9feb85346129d562d1bf44
Fructuss <Afanasev10056@gmail.com>
2023-07-05 20:05:01 +0300

    create user model for save jwt token and id

---
+++ Frontend/Desktop/AvaloniaChat.Desktop/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaChat.Desktop.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
