3bd425d9fe18c8cd5173f07c356586f44fe1091d
Fructuss <Afanasev10056@gmail.com>
2023-07-05 20:02:05 +0300

    adding a user in group on connection

--- Backend/AvaloniaChat.Backend/Hubs/ChatHub.cs
+++ Backend/AvaloniaChat.Backend/Hubs/ChatHub.cs
@@ -2,28 +2,21 @@
 using AvaloniaChat.Infrastructure;
 using AvaloniaChat.Domain;
 using AvaloniaChat.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 
 namespace AvaloniaChat.Backend.Hubs
 {
-    
+    [Authorize]
     public class ChatHub : Hub
     {
-        private string groupname = "tets";
+        
 
         private readonly ChatDbContext _chatDbContext;
         public ChatHub(ChatDbContext chatDbContext)
         {
             _chatDbContext = chatDbContext;
-        }
-        public Task JoinUserGroup()
-        {
-            return Groups.AddToGroupAsync(Context.ConnectionId, groupname);
-        }
-        public Task LeaveUserGroup()
-        {
-            return Groups.RemoveFromGroupAsync(Context.ConnectionId, groupname);
         }
 
         //public async Task SendMessage(string user, string message, Guid groupId)
@@ -40,23 +33,29 @@ namespace AvaloniaChat.Backend.Hubs
         /// <param name="user"></param>
         /// <returns></returns>
 
-        public async Task JoinChat(string user)
+        public async Task JoinChat(string user, string groupName)
         {
-            await Clients.Group(groupname).SendAsync("Join", user);
+            await Clients.Group(groupName).SendAsync("Join", user);
         }
         /// <summary>
         ///  when the user leave the chat
         /// </summary>
         /// <param name="user"></param>
         /// <returns></returns>
-        public async Task LeaveChat(string user)
+        public async Task LeaveChat(string user, string groupName)
         {
-            await Clients.Group(groupname).SendAsync("Leave", user);
+            await Clients.Group(groupName).SendAsync("Leave", user);
         }
 
         public override Task OnConnectedAsync()
         {
-            Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+            var userGroup = _chatDbContext.UserGroups.Where(x=> x.User.Username == Context.User.Identity.Name).Select(x => x.GroupId.ToString()).ToList();
+
+            foreach (var group in userGroup)
+            {
+            Groups.AddToGroupAsync(Context.ConnectionId, group);
+
+            }
             return base.OnConnectedAsync();
         }
     }
