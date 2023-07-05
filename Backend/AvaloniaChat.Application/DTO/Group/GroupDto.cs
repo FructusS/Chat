1ebfa1ac662a2547ef7d252262d9462537961845
Fructuss <Afanasev10056@gmail.com>
2023-07-05 20:06:25 +0300

    dto for group

---
+++ Backend/AvaloniaChat.Application/DTO/Group/GroupDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaChat.Application.DTO.Group
+{
+    public class GroupDto
+    {
+        public Guid GroupId { get; set; }
+        public string GroupName { get; set; }
+        public byte[] GroupLogo { get; set; }
+        public int UserGroupId { get; set; }
+    }
+}
