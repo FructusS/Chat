6ad6a46e6c58858ea5778aa30722c5e389e39adf
Fructuss <Afanasev10056@gmail.com>
2023-07-05 20:05:34 +0300

    create model for update group

---
+++ Backend/AvaloniaChat.Application/DTO/Group/UpdateGroupDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaChat.Application.DTO.Group
+{
+    public class UpdateGroupDto
+    {
+        public Guid GroupId { get; set; } 
+        public string GroupTitle { get; set; }
+        public byte[]? GroupImage { get; set; }
+    }
+}
