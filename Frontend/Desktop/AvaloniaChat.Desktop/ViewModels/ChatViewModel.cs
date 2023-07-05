e92d76b00f7140aff438dc4599f041198ff8edb5
Fructuss <Afanasev10056@gmail.com>
2023-07-05 20:18:56 +0300

    add load messages and user group, change send messages functional

--- Frontend/Desktop/AvaloniaChat.Desktop/ViewModels/ChatViewModel.cs
+++ Frontend/Desktop/AvaloniaChat.Desktop/ViewModels/ChatViewModel.cs
@@ -1,5 +1,5 @@
 ﻿using AvaloniaChat.Application.DTO.Message;
-using AvaloniaChat.Domain.Models;
+using AvaloniaChat.Application.DTO.UserGroup;
 using Microsoft.AspNetCore.SignalR.Client;
 using Prism.Commands;
 using System;
@@ -11,70 +11,122 @@ using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Controls.Selection;
+using AvaloniaChat.Application.DTO.Group;
+using AvaloniaChat.Desktop.Models;
+using AvaloniaChat.Domain.Models;
+using AvaloniaEdit.Editing;
+using ReactiveUI;
 
 namespace AvaloniaChat.Desktop.ViewModels
 {
-    public class ChatViewModel : ViewModelBase
+    public class ChatViewModel : ViewModelBase 
     {
 
-        private const string url = "http://localhost:5000/api/Messages/";
+        private const string baseUrl = "http://localhost:5000/api";
         private readonly HubConnection _hubConnection;
         private static readonly HttpClient _httpClient = new()
         {
-            BaseAddress = new Uri(url),
+            BaseAddress = new Uri(baseUrl),
         };
-        private ObservableCollection<MessageDto> messages = new();
+        private ObservableCollection<MessageDto> _messages = new();
         public ObservableCollection<MessageDto> Messages
         {
-            get { return messages; }
-            set
-            {
-                messages = value;
-                OnPropertyChanged();
-            }
+            get { return _messages; }
+            set { _messages = value; OnPropertyChanged(); }
         }
 
-        private string messageText { get; set; }
+
+        private ObservableCollection<GroupDto> _userGroup = new();
+        public ObservableCollection<GroupDto> UserGroups
+        {
+            get { return _userGroup; }
+            set { _userGroup = value; OnPropertyChanged(); }
+        }
+
+
+        private string _messageText;
 
         public string MessageText
         {
-            get { return messageText; }
-            set
-            {
-                messageText = value;
-                OnPropertyChanged();
-            }
+            get { return _messageText; }
+            set { _messageText = value; OnPropertyChanged(); }
         }
-
+        
+        private UserModel _userModel;
         public DelegateCommand SendCommand { get; }
-
-        public ChatViewModel()
+        public ChatViewModel(UserModel userModel)
         {
             SendCommand = new DelegateCommand(OnSend);
 
 
-
+            _userModel = userModel;
             _hubConnection = new HubConnectionBuilder()
                 .WithAutomaticReconnect()
-                .WithUrl($"http://localhost:5000/chatHub")
+                .WithUrl($"http://localhost:5000/chatHub", opt =>
+                {
+                    opt.AccessTokenProvider = () => Task.FromResult(_userModel.Token);
+                })
                 .Build();
+            Task.Run(() =>
+            {
+                Connect(); 
+                LoadUserGroups();
 
-            Connect();
-            LoadMessageHistory();
+            });
+
+
             _hubConnection.On<MessageDto>("ReceiveMessage", (message) =>
             {
                 Messages.Add(message);
             });
+            
 
+
+            Selection = new SelectionModel<GroupDto>();
+            Selection.SelectionChanged += SelectionChanged;
+
+
+        }
+
+        public SelectionModel<GroupDto> Selection { get; set; }
+
+        private void SelectionChanged(object? sender, SelectionModelSelectionChangedEventArgs<GroupDto> e)
+        {
+            if (Selection.SelectedItem != null)
+            {
+                LoadMessageHistory();
+            }
+        }
+
+        private async Task LoadUserGroups()
+        {
+            var userGroup = await _httpClient.GetFromJsonAsync<List<GroupDto>>($"{baseUrl}/Group/{_userModel.UserId}");
+
+            UserGroups = new ObservableCollection<GroupDto>(userGroup);
+
+            LoadMessageHistory();
 
         }
 
         private async void LoadMessageHistory()
         {
-            var messages = await _httpClient.GetFromJsonAsync<List<MessageDto>>($"{url}{1}");
+            List<MessageDto?> messages = new();
+            Messages.Clear();
+
+            if (Selection.SelectedItem == null)
+            {
+                messages = await _httpClient.GetFromJsonAsync<List<MessageDto>>($"{baseUrl}/Messages/{UserGroups[0].GroupId}");
+
+            }
+            else
+            {
+                messages = await _httpClient.GetFromJsonAsync<List<MessageDto>>($"{baseUrl}/Messages/{Selection.SelectedItem.GroupId}");
+            }
+           
             Messages = new ObservableCollection<MessageDto>(messages);
         }
-
         public async Task Connect()
         {
             try
@@ -89,24 +141,27 @@ namespace AvaloniaChat.Desktop.ViewModels
 
         private async void OnSend()
         {
+
             if (string.IsNullOrEmpty(MessageText))
                 return;
+
             var message = new CreateMessageDto()
             {
                 SendDate = DateTime.Now,
                 MessageText = MessageText,
-                UserGroupId = 1,
-                UserId = 2
+                GroupId = Selection.SelectedItem.GroupId,
+                UserId = _userModel.UserId
             };
-            using StringContent jsonContent = new(
 
+            using StringContent jsonContent = new(
                 JsonSerializer.Serialize(message),
                 Encoding.UTF8,
                 "application/json");
+
             try
             {
-                using HttpResponseMessage response = await _httpClient.PostAsync($"{url}create", jsonContent);
-                MessageText = "";
+                using HttpResponseMessage response = await _httpClient.PostAsync($"{baseUrl}/Messages/create", jsonContent);
+                MessageText = string.Empty;
             }
             catch (Exception e)
             {
