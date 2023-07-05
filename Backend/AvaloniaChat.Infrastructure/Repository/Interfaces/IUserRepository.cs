f7f7e7349b831cc558701daf54d72e85b8e80559
Fructuss <Afanasev10056@gmail.com>
2023-07-05 20:05:55 +0300

    interface for user repository

---
+++ Backend/AvaloniaChat.Infrastructure/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AvaloniaChat.Application.DTO.User;
+using AvaloniaChat.Domain.Models;
+
+namespace AvaloniaChat.Infrastructure.Repository.Interfaces;
+
+public interface IUserRepository
+{
+    Task<UserDto> CreateUser(CreateUserDto createUser);
+    Task<UserDto> UpdateUser(UpdateUserDto updateUser);
+    Task<User?> GetUserByUsername(string username);
+    Task DeleteUser(string username);
+    Task<bool> UserIsExistByUsername(string username);
+}
