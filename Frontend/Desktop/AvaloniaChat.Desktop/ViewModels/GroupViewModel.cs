fd6ad56c1663d79271d998a971c6d727421dfb42
Fructuss <Afanasev10056@gmail.com>
2023-07-07 22:12:38 +0300

    testing adding group

--- Frontend/Desktop/AvaloniaChat.Desktop/ViewModels/GroupViewModel.cs
+++ Frontend/Desktop/AvaloniaChat.Desktop/ViewModels/GroupViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using AvaloniaChat.Application.DTO.Auth;
 using AvaloniaChat.Application.DTO.Group;
+using DynamicData.Tests;
 
 namespace AvaloniaChat.Desktop.ViewModels
 {
@@ -17,6 +21,25 @@ namespace AvaloniaChat.Desktop.ViewModels
         };
         public  GroupViewModel()
         {
+            Test();
+           
+        }
+
+        private async void Test()
+        {
+            var group = new CreateGroupDto()
+            {
+                GroupId = Guid.NewGuid(),
+                GroupImage = ImageToByteArrayFromFilePath("seats.jpg"),
+                GroupTitle = "group with image"
+            };
+            using StringContent jsonContent = new(
+
+                JsonSerializer.Serialize(group),
+                Encoding.UTF8,
+                "application/json");
+            using HttpResponseMessage response = await _httpClient.PostAsync($"{url}/create", jsonContent);
+
             
         }
 
@@ -28,6 +51,12 @@ namespace AvaloniaChat.Desktop.ViewModels
         public GroupViewModel(Guid groupId)
         {
             throw new NotImplementedException();
+        }
+
+        public static byte[] ImageToByteArrayFromFilePath(string imagefilePath)
+        {
+            byte[] imageArray = File.ReadAllBytes(imagefilePath);
+            return imageArray;
         }
     }
 }
