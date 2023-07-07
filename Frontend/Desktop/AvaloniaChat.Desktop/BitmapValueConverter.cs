b319deb213fe70c95208dd3096bd1a6396f00297
Fructuss <Afanasev10056@gmail.com>
2023-07-07 22:16:40 +0300

    converter byte array to bitmap

---
+++ Frontend/Desktop/AvaloniaChat.Desktop/BitmapValueConverter.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+using System.IO;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace AvaloniaChat.Desktop
+{
+    public class BitmapValueConverter : IValueConverter
+    {
+        public static BitmapValueConverter Instance = new BitmapValueConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return null;
+            if (value is byte[] rawBytes && targetType == typeof(IImage))
+            {
+                if (rawBytes.Length > 0)
+                {
+                    MemoryStream ms = new MemoryStream(rawBytes);
+                    return new Bitmap(ms);
+                }
+
+                return null;
+
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
