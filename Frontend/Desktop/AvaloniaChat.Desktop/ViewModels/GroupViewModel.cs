32849cb1dfcd882cdce0dda82692e58fcbb26912
FructusS <afanasev10056@gmail.com>
2023-07-08 22:10:17 +0300

    add group name and logo field

--- Frontend/Desktop/AvaloniaChat.Desktop/ViewModels/GroupViewModel.cs
+++ Frontend/Desktop/AvaloniaChat.Desktop/ViewModels/GroupViewModel.cs
@@ -6,23 +6,83 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 using AvaloniaChat.Application.DTO.Auth;
 using AvaloniaChat.Application.DTO.Group;
+using AvaloniaChat.Desktop.Events;
 using DynamicData.Tests;
+using Prism.Commands;
+using Prism.Events;
 
 namespace AvaloniaChat.Desktop.ViewModels
 {
     public class GroupViewModel : ViewModelBase
     {
+        private readonly IEventAggregator _eventAggregator;
         private const string url = "http://localhost:5000/api/Group";
         private static readonly HttpClient _httpClient = new()
         {
             BaseAddress = new Uri(url),
         };
-        public  GroupViewModel()
+        public DelegateCommand BackCommand { get; }
+
+
+        private string _groupName;
+
+        public string GroupName
         {
-            Test();
-           
+            get => _groupName;
+            set
+            {
+                _groupName = value; OnPropertyChanged();
+            }
+        }
+        private Bitmap _groupLogo;
+
+        public Bitmap GroupLogo
+        {
+            get => _groupLogo;
+            set
+            {
+                _groupLogo = value; OnPropertyChanged();
+            }
+        }
+
+
+
+        /// <summary>
+        /// update group constructor
+        /// </summary>
+        /// <param name="groupDto"></param>
+        /// <param name="eventAggregator"></param>
+        public GroupViewModel(UpdateGroupDto groupDto, IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            BackCommand = new DelegateCommand(OnBackButtonClick);
+        }
+
+        /// <summary>
+        /// delete group constructor
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <param name="eventAggregator"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public GroupViewModel(Guid groupId, IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            BackCommand = new DelegateCommand(OnBackButtonClick);
+        }
+        /// <summary>
+        /// create group constructor
+        /// </summary>
+        /// <param name="eventAggregator"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public GroupViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            BackCommand = new DelegateCommand(OnBackButtonClick);
+
+           // GroupLogo = new Bitmap("../")
         }
 
         private async void Test()
@@ -39,18 +99,11 @@ namespace AvaloniaChat.Desktop.ViewModels
                 Encoding.UTF8,
                 "application/json");
             using HttpResponseMessage response = await _httpClient.PostAsync($"{url}/create", jsonContent);
-
-            
         }
 
-        public GroupViewModel(UpdateGroupDto updateGroupDto)
+        private void OnBackButtonClick()
         {
-            throw new NotImplementedException();
-        }
-
-        public GroupViewModel(Guid groupId)
-        {
-            throw new NotImplementedException();
+            _eventAggregator.GetEvent<NavigateToChatEvent>().Publish();
         }
 
         public static byte[] ImageToByteArrayFromFilePath(string imagefilePath)
