1f6ab9255d391c867a5fcf015d11ab487a96e952
FructusS <afanasev10056@gmail.com>
2023-07-17 00:01:41 +0300

    base response

---
+++ Frontend/Desktop/AvaloniaChat.Desktop/Models/BaseResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaChat.Desktop.Models
+{
+    public class BaseResponse<T>
+    {
+        public string ErrorText { get; set; }
+        public bool IsError { get; set; }
+
+        public T Data { get; set; }
+        public BaseResponse(string errorText, bool isError, T data)
+        {
+            ErrorText = errorText;
+            IsError = isError;
+            Data = data;
+        }
+    }
+
+}
