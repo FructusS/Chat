f6119b2d4735ed524add3849faaa476f4574a694
Fructuss <Afanasev10056@gmail.com>
2023-08-04 11:21:11 +0300

    add dto for create group

---
+++ Backend/AvaloniaChat.Application/DTO/UserGroup/CreateUserGroupDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaChat.Application.DTO.UserGroup
+{
+    public class CreateUserGroupDto
+    {
+        public Guid GroupId { get; set;}
+        public int UserId { get; set;}
+    }
+}
