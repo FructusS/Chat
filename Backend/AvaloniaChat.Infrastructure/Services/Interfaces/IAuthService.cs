fd3d0bbecf9110f7d85808dd911618e3d50d33a4
Fructuss <Afanasev10056@gmail.com>
2023-08-04 10:07:21 +0300

    change param from claims to auth request in generate access token

--- Backend/AvaloniaChat.Infrastructure/Services/Interfaces/IAuthService.cs
+++ Backend/AvaloniaChat.Infrastructure/Services/Interfaces/IAuthService.cs
@@ -1,11 +1,13 @@
 ﻿using System.Security.Claims;
+using AvaloniaChat.Application.DTO.Auth;
 
 namespace AvaloniaChat.Backend.Services.Interfaces
 {
     public interface IAuthService
     {
-        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateAccessToken(AuthRequest userModel);
         string GenerateRefreshToken();
         ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+        string ValidateJwtToken(string jwtToken);
     }
 }
