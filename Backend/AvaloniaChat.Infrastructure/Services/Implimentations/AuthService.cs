625affcec62225729f45ef363da9a4261c0c39b7
Fructuss <Afanasev10056@gmail.com>
2023-08-04 09:59:05 +0300

    add validation jwt method

--- Backend/AvaloniaChat.Infrastructure/Services/Implimentations/AuthService.cs
+++ Backend/AvaloniaChat.Infrastructure/Services/Implimentations/AuthService.cs
@@ -1,7 +1,9 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
 using AvaloniaChat.Application.Configs;
+using AvaloniaChat.Application.DTO.Auth;
 using AvaloniaChat.Backend.Services.Interfaces;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
@@ -18,8 +20,13 @@ namespace AvaloniaChat.Infrastructure.Services.Implimentations
             _jwtConfig = jwtConfig.Value;
         }
 
-        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        public string GenerateAccessToken(AuthRequest loginModel)
         {
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Name, loginModel.Username),
+                new(ClaimsIdentity.DefaultRoleClaimType, "user")
+            };
             var tokeOptions = new JwtSecurityToken(
                 issuer: _jwtConfig.Issuer,
                 audience: _jwtConfig.Audience,
@@ -29,6 +36,36 @@ namespace AvaloniaChat.Infrastructure.Services.Implimentations
             );
             var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
             return tokenString;
+        }
+        public string ValidateJwtToken(string token)
+        {
+            if (token == null)
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = _jwtConfig.SymmetricSecurityKey(),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var username = (jwtToken.Claims.First(x => x.Type == "Username").Value);
+
+                // return user id from JWT token if validation successful
+                return username;
+            }
+            catch
+            {
+                // return null if validation fails
+                return null;
+            }
         }
         public string GenerateRefreshToken()
         {
