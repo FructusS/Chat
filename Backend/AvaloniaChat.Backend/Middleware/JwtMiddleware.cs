ca7077c17e44b4cd486bb33f8b6e6f56b07ca37d
Fructuss <Afanasev10056@gmail.com>
2023-08-04 11:20:53 +0300

    jwt middleware

---
+++ Backend/AvaloniaChat.Backend/Middleware/JwtMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using AvaloniaChat.Application.Configs;
+using AvaloniaChat.Backend.Services.Interfaces;
+using AvaloniaChat.Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace AvaloniaChat.Backend.Middleware
+{
+    public class JwtMiddleware
+    {
+        private readonly JwtConfig _jwtConfig;
+        private readonly RequestDelegate _next;
+        public JwtMiddleware(IOptions<JwtConfig> jwtConfig, RequestDelegate next)
+        {
+            _next = next;
+            _jwtConfig = jwtConfig.Value;
+        }
+        public async Task Invoke(HttpContext context, IUserService userService, IAuthService authService)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var username = authService.ValidateJwtToken(token);
+            if (username != null)
+            {
+                // attach user to context on successful jwt validation
+                context.Items["User"] = userService.GetUserByUsername(username);
+            }
+
+            await _next(context);
+        }
+    }
+}
