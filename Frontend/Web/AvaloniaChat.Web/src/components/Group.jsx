81b03609efb21330ad7dce132e68ef4daddbfd23
FructusS <afanasev10056@gmail.com>
2023-08-14 10:01:55 +0300

    change styles group  component

--- Frontend/Web/AvaloniaChat.Web/src/components/Group.jsx
+++ Frontend/Web/AvaloniaChat.Web/src/components/Group.jsx
@@ -1,21 +1,43 @@
-export default function Group({ group }) {
-  return (
-    //   <div
-    //     // className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
-    //     // <img
-    //     //   className="chat-bubble__left"
-    //     //   src={message.avatar}
-    //     //   alt="user avatar"
-    //     // />
-    //     // <div className="chat-bubble__right">
-    //   <p className="user-name">{message.name}</p>
-    //   <p className="user-message">{message.text}</p>
-    //     // </div>
-    //   </div>
-    <div>
-      <img src={`data:image/jpeg;base64,${group.groupLogo}`} />
-      <p className="user-name">{group.groupName}</p>
-    </div>
-    
-  );
+import logo from "../logo.svg"
+export default function Group({ onGroupClick, group }) {
+    return (
+        //   <div
+        //     // className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
+        //     // <img
+        //     //   className="chat-bubble__left"
+        //     //   src={message.avatar}
+        //     //   alt="user avatar"
+        //     // />
+        //     // <div className="chat-bubble__right">
+        //   <p className="user-name">{message.name}</p>
+        //   <p className="user-message">{message.text}</p>
+        //     // </div>
+        //   </div>
+
+        <div class="d-flex justify-content-between">
+            <div class="d-flex flex-row">
+                <a
+                    href="#!"
+                    class="d-flex justify-content-between text-decoration-none"
+                    onClick={() => {
+                        onGroupClick(group.groupId);
+                    }}>
+                    <img
+                        src={
+                            group.groupLogo != null
+                                ? `data:image/jpeg;base64,${group.groupLogo}`
+                                : logo
+                        }
+                        alt="avatar"
+                        class="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                        width="54"
+                        height="54"
+                    ></img>
+                    <p class="justify-content-center mb-0 align-baseline">
+                        {group.groupName}
+                    </p>
+                </a>
+            </div>
+        </div>
+    );
 }
