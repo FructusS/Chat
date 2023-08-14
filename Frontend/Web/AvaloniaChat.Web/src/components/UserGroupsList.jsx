94daa5158921ef3e75546bf195899e62e37cc3ac
FructusS <afanasev10056@gmail.com>
2023-08-14 10:05:22 +0300

    changes

--- Frontend/Web/AvaloniaChat.Web/src/components/UserGroupsList.jsx
+++ Frontend/Web/AvaloniaChat.Web/src/components/UserGroupsList.jsx
@@ -1,32 +1,40 @@
-import { useEffect } from "react";
+import {useEffect} from "react";
 import axios from "axios";
-import { useState } from "react";
-import { BASE_URL } from "../constants";
+import {useState} from "react";
+import {BASE_URL} from "../constants";
 import Group from "./Group";
-export default function UserGroupsList() {
-  const [groups, setGroups] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`/UserGroup/${localStorage.getItem("userId")}`)
-      .then(function (response) {
-        if (response.status == 200) {
-          setGroups(response.data);
-          return;
-        } else {
-          return;
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-        return;
-      });
-  }, []);
 
-  return (
-    <div className="messages-wrapper">
-      {groups?.map((group) => (
-        <Group key={group.id} group={group} />
-      ))}
-    </div>
-  );
+export default function UserGroupsList({onGroupClick}) {
+    const [groups, setGroups] = useState([]);
+    useEffect(() => {
+        axios
+            .get(`${BASE_URL}UserGroup/${sessionStorage.getItem("userId")}`)
+            .then(function (response) {
+                if (response.status === 200) {
+                    setGroups(response.data.data);
+                    return;
+                } else {
+                    return;
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                return;
+            });
+    }, []);
+
+    return (
+        {groups?.map(
+            (group) => (
+                <Group
+                    onGroupClick={onGroupClick}
+                    key={group.groupId}
+                    group={group}
+                />
+            )
+        )
+}
+</div>
+)
+    ;
 }
