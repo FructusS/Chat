5bd813f3de2651edfbc70233d9e0d31570795529
Mihkail Afanasev <afanasev10056@gmail.com>
2023-08-15 16:28:41 +0300

    feat: add configuration for user table

---
+++ Backend/AvaloniaChat.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvaloniaChat.Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AvaloniaChat.Infrastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasData
+            (
+                new User
+                {
+                    UserId = 1,
+                    Username = "test1",
+                   PasswordHash = BCrypt.Net.BCrypt.HashPassword("test1"),
+                   
+                },
+                new User
+                {
+                    UserId = 2,
+                    Username = "test2",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("test2"),
+
+                },
+                new User
+                {
+                    UserId = 3,
+                    Username = "test3",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("test3"),
+
+                }
+            );
+        }
+    }
+}
