1be7c951d7143046be2af3a441791a45350b9ab2
Mihkail Afanasev <afanasev10056@gmail.com>
2023-08-15 16:29:43 +0300

    feat: add migration for user admin in group

---
+++ Backend/AvaloniaChat.Infrastructure/Migrations/20230815091123_add_user_admin_id_column_to_group_table.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvaloniaChat.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_user_admin_id_column_to_group_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_UserGroups_UsergroupId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_UsergroupId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "UsergroupId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SendDate",
+                table: "Messages",
+                type: "timestamp(0) without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "GroupId",
+                table: "Messages",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Messages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "user_admin",
+                table: "Groups",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_GroupId",
+                table: "Messages",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserId",
+                table: "Messages",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_user_admin",
+                table: "Groups",
+                column: "user_admin");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Users_user_admin",
+                table: "Groups",
+                column: "user_admin",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Groups_GroupId",
+                table: "Messages",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_UserId",
+                table: "Messages",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "UserGroups");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Users_user_admin",
+                table: "Groups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Groups_GroupId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_UserId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_GroupId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_UserId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_user_admin",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "user_admin",
+                table: "Groups");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SendDate",
+                table: "Messages",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(0) without time zone");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsergroupId",
+                table: "Messages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UsergroupId",
+                table: "Messages",
+                column: "UsergroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_UserGroups_UsergroupId",
+                table: "Messages",
+                column: "UsergroupId",
+                principalTable: "UserGroups",
+                principalColumn: "UsergroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
