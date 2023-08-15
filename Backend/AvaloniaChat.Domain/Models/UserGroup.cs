9899e2400a2790ab10e3c0f0bb8714f3958c59bd
Mihkail Afanasev <afanasev10056@gmail.com>
2023-08-15 16:24:28 +0300

    refactor: delete nullable property
    ra

--- Backend/AvaloniaChat.Domain/Models/UserGroup.cs
+++ Backend/AvaloniaChat.Domain/Models/UserGroup.cs
@@ -10,10 +10,10 @@ namespace AvaloniaChat.Domain.Models
         {
         }
 
-        public int? UserId { get; set; }
-        public Guid? GroupId { get; set; }
-        public int UsergroupId { get; set; }
-        public virtual Group? Group { get; set; }
-        public virtual User? User { get; set; }
+        public int UserId { get; set; }
+        public Guid GroupId { get; set; }
+        public int UserGroupId { get; set; }
+        public virtual Group Group { get; set; }
+        public virtual User User { get; set; }
     }
 }
