0c956069558fe43990699839609f97fe77eca249
Mihkail Afanasev <afanasev10056@gmail.com>
2023-08-19 14:15:00 +0300

    refactor: change routes

--- Frontend/Web/AvaloniaChat.Web/src/App.js
+++ Frontend/Web/AvaloniaChat.Web/src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navigate } from "react-router-dom";
-import { LoginPage, ChatPage } from "./pages";
+import { Chat, Home } from "./pages";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { useState } from "react";
+
 function App() {
+    // const [user,setUser] = useState()
+    // setUser(sessionStorage.getItem("accessToken"))
+    // console.log(user)
     return (
         <div className="App">
             <Router>
                 <div className="container-md py-5">
                     <Routes>
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/chat" element={<ChatPage />} />
-
-                        <Route
-                            path="*"
-                            element={<Navigate to="/login" replace={true} />}
-                        />
+                        <Route path="/" element={<Home />} />
+                        <Route path="/chat" element={<Chat />} />
+                        <Route path="*" element={ <div>Not found</div>}></Route>
                     </Routes>
                 </div>
             </Router>
