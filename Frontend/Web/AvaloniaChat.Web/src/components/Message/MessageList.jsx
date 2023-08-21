7a75549e28c97ebb1a705c82f5439e7bbd638557
Mihkail Afanasev <afanasev10056@gmail.com>
2023-08-21 11:52:41 +0300

    refacotr: rename class to classname

--- Frontend/Web/AvaloniaChat.Web/src/components/Message/MessageList.jsx
+++ Frontend/Web/AvaloniaChat.Web/src/components/Message/MessageList.jsx
@@ -1,18 +1,18 @@
 export const MessageList = ({ messages }) => {
     return (
-            <ul class="list-unstyled" data-mdb-perfect-scrollbar="true"  data-mdb-suppress-scroll-x="true" style={{height: '400px' , position: 'relative'}}>
+            <ul className="list-unstyled" data-mdb-perfect-scrollbar="true"  data-mdb-suppress-scroll-x="true" style={{height: '400px' , position: 'relative'}}>
                 {messages?.map(
                     (message) => (
                         <li >
-                            <div class="card">
-                                <div class="card-header d-flex justify-content-between">
-                                    <p class="fw-bold mb-0">{message.username}</p>
-                                    <p class="text-muted small mb-0">
-                                        <i class="far fa-clock"></i> {message.sendDate}
+                            <div className="card">
+                                <div className="card-header d-flex justify-content-between">
+                                    <p className="fw-bold mb-0">{message.username}</p>
+                                    <p className="text-muted small mb-0">
+                                        <i className="far fa-clock"></i> {message.sendDate}
                                     </p>
                                 </div>
-                                <div class="card-body">
-                                    <p class="mb-0">
+                                <div className="card-body">
+                                    <p className="mb-0">
                                         {message.messageText}
                                     </p>
                                 </div>
