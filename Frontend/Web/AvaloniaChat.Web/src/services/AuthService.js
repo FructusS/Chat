db43ad7880950d65876a66519ec40fe19a78f85c
Mihkail Afanasev <afanasev10056@gmail.com>
2023-08-21 11:37:12 +0300

    feat!: add login fun

--- Frontend/Web/AvaloniaChat.Web/src/services/AuthService.js
+++ Frontend/Web/AvaloniaChat.Web/src/services/AuthService.js
@@ -1,19 +1,8 @@
-
-
-class AuthService{
-    login(username, password) {
-        return axios
-          .post(BASE_URL + "/Auth/login", {
-            username,
-            password
-          })
-          .then(response => {
-            if (response.data.accessToken) {
-              localStorage.setItem("accessToken", response.data.data.accessToken);
-            }
-            return response.data;
-          });
-      }
-
-}   
-export default new AuthService();
+import axios from "axios";
+import { BASE_URL } from "../constants";
+export const login = async (username, password) => {
+    return axios.post(`${BASE_URL}/Auth`, {
+        username,
+        password,
+    });
+};
