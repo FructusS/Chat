4a8253f526e2d6ac4f523093d504132483eb866d
Mihkail Afanasev <afanasev10056@gmail.com>
2023-11-28 09:43:03 +0300

    add connect to signalr hub and get messages

--- Frontend/Web/AvaloniaChat.Web/src/pages/home/Home.jsx
+++ Frontend/Web/AvaloniaChat.Web/src/pages/home/Home.jsx
@@ -1,15 +1,63 @@
 import { useState, useEffect } from "react";
-import { Login,Chat } from "../../pages";
+import "../../App.css";
+import { getMessages } from "../../services/MessageService";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import { MessageList, GroupList } from "../../components";
 export const Home = () => {
+    const [connection, setConnection] = useState();
+    const [messages, setMessages] = useState([]);
+    const joinRoom = async () => {
+        try {
+            const connection = new HubConnectionBuilder()
+                .withUrl("http://localhost:5000/chatHub", {
+                    accessTokenFactory: () => {
+                        return sessionStorage.getItem("accessToken");
+                    },
+                })
+                .configureLogging(LogLevel.Information)
+                .withAutomaticReconnect()
+                .build();
+            await connection.start().catch((e) => {
+                console.log(e);
+            });
+            connection.on("ReceiveMessage", (message) => {
+                setMessages((messages) => [...messages, message]);
+            });
 
+            connection.onclose((e) => {
+                console.log(e);
+                setConnection();
+                setMessages([]);
+            });
 
-    const [userId, setUserId] = useState();
-    
-    useEffect(() => {
-        setUserId(sessionStorage.getItem("userId"))
-    });
+            setConnection(connection);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    async function onGroupClick(groupId) {
+        try {
+            console.log(groupId);
+
+            const result = await getMessages(groupId);
+            setMessages(result.data.data);
+        } catch (e) {
+            //  setError(e?.response?.data?.error?.message || e?.message);
+            console.log(e?.response?.data?.error?.message || e?.message);
+        }
+        await joinRoom();
+    }
 
     return (
-        <Login></Login>
+        <div className="flex-wrapper ">
+            <div className="group-list-container">
+                <GroupList onGroupClick={onGroupClick}></GroupList>
+            </div>
+
+            <div className="message-list-container">
+                <MessageList messages={messages}></MessageList>
+            </div>
+        </div>
     );
 };
