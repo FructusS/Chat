a489c97562326dbe39cfb69bce6325ec318d9e47
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-25 11:49:04 +0300

    feat: add jwt service with GenerateRefreshToken, GetPrincipalFromExpiredToken,GenerateAccessToken,ValidateJwtToken

---
+++ Backend/AvaloniaChat.Backend/Business/Services/Interfaces/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using AvaloniaChat.Application.DTO.Auth;
+using System.Security.Claims;
+
+namespace AvaloniaChat.Backend.Business.Services.Interfaces
+{
+    public interface IJwtService
+    {
+        string GenerateAccessToken(AuthRequest userModel);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+        string ValidateJwtToken(string jwtToken);
+
+    }
+}
