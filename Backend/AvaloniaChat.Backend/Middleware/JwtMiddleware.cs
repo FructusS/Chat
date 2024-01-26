1b51aa6da9621e48e4b75ca86d45acd91832bba6
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-26 14:57:42 +0300

    refactor: throw NotFoundException when token is null or empty

--- Backend/AvaloniaChat.Backend/Middleware/JwtMiddleware.cs
+++ Backend/AvaloniaChat.Backend/Middleware/JwtMiddleware.cs
@@ -4,6 +4,7 @@ using AvaloniaChat.Backend.Business.Services.Interfaces;
 using AvaloniaChat.Backend.Services.Interfaces;
 using AvaloniaChat.Infrastructure.Services.Interfaces;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AvaloniaChat.Backend.Middleware
 {
@@ -19,13 +20,13 @@ namespace AvaloniaChat.Backend.Middleware
         public async Task Invoke(HttpContext context, IUserService userService, IJwtService jwtService)
         {
             var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            var username = authService.ValidateJwtToken(token);
-            if (username != null)
+            if (token.IsNullOrEmpty())
             {
-                // attach user to context on successful jwt validation
-                context.Items["User"] = userService.GetUserByUsername(username);
+                throw new NotFoundException("Token not found");
             }
 
+            var username = jwtService.ValidateJwtToken(token);
+            context.Items["User"] = userService.GetUserByUsername(username);
             await _next(context);
         }
     }
