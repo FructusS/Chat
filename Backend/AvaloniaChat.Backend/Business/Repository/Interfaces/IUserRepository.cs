794ba92705cc446b49b321c5e3e262c699b90e6a
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-26 15:09:20 +0300

    refactor: return nullable UserDto in GetUser method

--- Backend/AvaloniaChat.Backend/Business/Repository/Interfaces/IUserRepository.cs
+++ Backend/AvaloniaChat.Backend/Business/Repository/Interfaces/IUserRepository.cs
@@ -7,7 +7,7 @@ public interface IUserRepository
 {
     Task<UserDto> CreateUser(CreateUserDto createUser);
     Task<UserDto> UpdateUser(UpdateUserDto updateUser);
-    Task<UserDto> GetUser(int userId);
+    Task<UserDto?> GetUser(int userId);
     Task<User?> GetUserByUsername(string username);
     Task DeleteUser(string username);
     Task<bool> UserIsExistByUsername(string username);
